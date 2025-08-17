d3f3219e03f1847eea9f26b1f5ea07fb46bfd5e8
alvaro <alvaro@gmail.com>
2025-08-16 20:07:19 -0400

    CRUD listo

--- MUNI/gestion_usuario/form.py
+++ MUNI/gestion_usuario/form.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from .models import Funcionario
 
+#creando el formulario para el modelo Funcionario 
 class FuncionarioForm(ModelForm):
     class Meta:
         model = Funcionario
--- MUNI/gestion_usuario/views.py
+++ MUNI/gestion_usuario/views.py
@@ -9,5 +9,25 @@ def index(request):
     form = FuncionarioForm()
     funcionario = Funcionario.objects.all()
     context['funcionario'] = funcionario
+    if request.method == "POST":
+        if 'Save' in request.POST:
+            pk= request.POST.get('Save')
+            if not pk:
+                form = FuncionarioForm(request.POST)
+                
+            else:
+                funcionario = Funcionario.objects.get(id=pk)
+                form = FuncionarioForm(request.POST, instance=funcionario)
+            form.save()
+            form = FuncionarioForm()
+        elif 'Delete' in request.POST:
+            pk= request.POST.get('Delete')
+            funcionario = Funcionario.objects.get(id=pk)
+            funcionario.delete()
+        elif 'Edit' in request.POST:
+            pk= request.POST.get('Edit')
+            funcionario = Funcionario.objects.get(id=pk)
+            form = FuncionarioForm(instance=funcionario)
+            funcionario = form
     context['form'] = form
     return render(request, 'gestion_usuario/index.html', context)
